4abbb56903323b067aee4d875ec2d0ce9b39918e
Maks-drzezdzon <maksdrzezdzon@gmail.com>
2020-08-25 23:24:11 +0100

    adding path and df

--- tmp.r
+++ tmp.r
@@ -4,4 +4,7 @@ b <- 1:10
 print(b)
 frame <- data.frame(a,b)
 print(frame)
-install.packages("car", "ggplot2", "pastecs", "psych");
+setwd('/Users/maksdrzezdzon/Documents/Github/R/data')
+df <- read.delim("Chapter 5/DownloadFestival(No Outlier).dat", header=TRUE)
+print(df)
+
