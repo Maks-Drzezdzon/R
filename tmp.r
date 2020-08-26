1bb31badcb1d26e334ce4669b0463b5d3afc9eb8
maksthorn <maksdrzezdzon@gmail.com>
2020-08-26 21:49:14 +0100

    adding comments and looking over graph functions

--- tmp.r
+++ tmp.r
@@ -16,7 +16,22 @@ setwd('C:/Users/Grim/Documents/GitHub/R/data')
 df <- read.delim("Chapter 5/DownloadFestival(No Outlier).dat", header=TRUE)
 print(df)
 
-hist.day1 <- ggplot(df, aes(day1)) + theme(legend.position = "none") +
+# plot
+hist.day1 <- ggplot(df, aes(day1)) + 
+  # remove legend
+  theme(legend.position = "none") +
+  # use density plot, color black and fill white
   geom_histogram(aes(y = ..density..), colour = "black", fill = "white") +
+  # name x/y labels
   labs(x = "Hygiene score on day 1", y = "Density")
+# render
 hist.day1
+
+# add curve
+hist.day1 +
+# The stat_function() command draws the normal curve using the function dnorm(). This
+# function basically returns the probability (i.e., the density) for a given value from a normal
+# distribution of known mean and standard deviation
+stat_function(fun = dnorm, args = list(mean = mean(df$day1, na.rm = TRUE),
+sd = sd(df$day1, na.rm = TRUE)),
+colour = "black", size = 1)
