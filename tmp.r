acc7c5e2a475a5ef2ad9f81ae6a4c97c061da751
maksthorn <maksdrzezdzon@gmail.com>
2020-08-30 17:16:06 +0100

    pushing changes to synce with Rstudio

--- tmp.r
+++ tmp.r
@@ -10,13 +10,16 @@ library(psych)
 library(Rcmdr)
 # tidyverse has most needed libs
 library(tidyverse)
-
+library(nycflights13)
 
 # setwd('/Users/maksdrzezdzon/Documents/Github/R/data')
 setwd('C:/Users/Grim/Documents/GitHub/R/data')
-
-df <- read.delim("Chapter 5/DownloadFestival(No Outlier).dat", header=TRUE)
-print(df)
-
-
+# df <- read.delim("Chapter 5/DownloadFestival(No Outlier).dat", header=TRUE)
+# print(df)
 # page 205 -- cont with R course
+# alt + shift + K
+
+# dplyr overwrites some functions in R
+# to use base funcs, you need to call them as shown below
+stats::filter()
+stats::lag()
