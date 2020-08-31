b64d272a93f29df4e18e549923a6dba27b7d4e75
Maks-drzezdzon <maksdrzezdzon@gmail.com>
2020-08-31 21:29:25 +0100

    fixed merge issues pushing again

--- tmp.r
+++ tmp.r
@@ -12,17 +12,11 @@ library(Rcmdr)
 library(tidyverse)
 library(nycflights13)
 
-<<<<<<< HEAD
 setwd('/Users/maksdrzezdzon/Documents/Github/R')
 # setwd('C:/Users/Grim/Documents/GitHub/R/data')
-=======
-# setwd('/Users/maksdrzezdzon/Documents/Github/R/data')
-setwd('C:/Users/Grim/Documents/GitHub/R/data')
-# df <- read.delim("Chapter 5/DownloadFestival(No Outlier).dat", header=TRUE)
-# print(df)
+
 # page 205 -- cont with R course
 # alt + shift + K
->>>>>>> b572bb4ff4ea5f12ee62f4358a3bf2585e87eef6
 
 # dplyr overwrites some functions in R
 # to use base funcs, you need to call them as shown below
