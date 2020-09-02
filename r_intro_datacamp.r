22caa3412c92239100454553b1f4a6d767367f0c
maksthorn <maksdrzezdzon@gmail.com>
2020-09-02 22:29:00 +0100

    adding link

--- r_intro_datacamp.r
+++ r_intro_datacamp.r
@@ -2,6 +2,7 @@ library(gapminder)
 library(dplyr)
 
 # %>% is like a pipe in linux 
+# https://campus.datacamp.com/courses/introduction-to-the-tidyverse/data-wrangling-1?ex=7
 gapminder <- gapminder::gapminder
 gapminder %>% filter(year == 2007, country == "United States")
 gapminder %>% arrange(gdpPercap)
